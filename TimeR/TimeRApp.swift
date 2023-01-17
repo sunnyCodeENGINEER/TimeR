d98896e12876ad785ac537af7eff40da860df0b8
Emmanuel Donkor <sunnycodeengineer@gmail.com>
2023-01-17 08:41:39 +0000

    Initial Commit

---
+++ TimeR/TimeRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeRApp.swift
+//  TimeR
+//
+//  Created by Emmanuel Donkor on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TimeRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
